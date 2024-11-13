19c2aff9f6e1c4f12b79162485eb095ea7de6140
Uzair Khan <uzair.3@live.com>
2024-11-13 22:07:51 +1100

    update API base URL to production endpoint

--- frontend/dapp-frontend/src/services/api.ts
+++ frontend/dapp-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000', // Replace with your API's base URL
+    baseURL: 'https://api.uzairkhan.me', // Replace with your API's base URL
     headers: {
       'Content-Type': 'application/json',
     },
